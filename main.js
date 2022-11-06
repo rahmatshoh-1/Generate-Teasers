5210e0d71184bfadefa0037f1e6143a4974393c7
rahmatshoh-1 <56288392+rahmatshoh-1@users.noreply.github.com>
2022-11-06 11:32:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
 // Our variables 
 let randomAnimals,  randomAlimalsBody, randomAdjactives,
-	btnGenerate, 	randomPartBody,    text, result
+    btnGenerate,    randomPartBody,    text, result
 
 // call classes from doc HTML
 btnGenerate = document.querySelector('.btnGenerate'),
-	   text = document.querySelector('.text')
+       text = document.querySelector('.text')
 
 // generateRandomWords from arrays
 const generateRandomWords = (arr) => {
-	return arr[Math.floor(Math.random() * arr.length)]
+    return arr[Math.floor(Math.random() * arr.length)]
 }
 // create generateDroznilok function
 const generateDroznilok = () => {
 // Our arrays
-	randomPartBody    = ['рука','нога','голова','волосы','глаза','нос','череп'],
-	randomAdjactives  = ['огромнная','маленькая','красивая','уродливая','вонючая','унылая','дурацкая'],
-	randomAlimalsBody = ['лапа','башка','левая лапа','усик']
-	randomAnimals     = ['тигра','собаки','слона','кошки','льва'],
+     randomPartBody    = ['рука','нога','голова','волосы','глаза','нос','череп'],
+     randomAdjactives  = ['огромнная','маленькая','красивая','уродливая','вонючая','унылая','дурацкая'],
+     randomAlimalsBody = ['лапа','башка','левая лапа','усик']
+     randomAnimals     = ['тигра','собаки','слона','кошки','льва'],
 // result
-	result = ["У тебя ", generateRandomWords(randomPartBody)," еще более ", generateRandomWords(randomAdjactives)+",чем ",generateRandomWords(randomAlimalsBody),' у ',generateRandomWords(randomAnimals)].join(" ")
+      result = ["У тебя ", generateRandomWords(randomPartBody)," еще более ", generateRandomWords(randomAdjactives)+",чем ",generateRandomWords(randomAlimalsBody),' у ',generateRandomWords(randomAnimals)].join(" ")
 // add result to paragraph text
-	text.innerText = result
+      text.innerText = result
 }
 // Generate Start
 btnGenerate.addEventListener("click",() => generateDroznilok())
